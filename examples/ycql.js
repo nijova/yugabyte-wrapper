d5d664cc2585030038568d752305d964450f59eb
nijova <niko.jovan@protonmail.com>
2022-02-10 18:30:34 -0500

    ycql example works

--- examples/ycql.js
+++ examples/ycql.js
@@ -1,3 +1,51 @@
-let loadBalancing = new cassandraDriver.policies.loadBalancing.RoundRobinPolicy();
+const YCQL = require('../index.js').YCQL;
+const async = require('async');
+const assert = require('assert');
+
+// Make sure your credentials and options allow you to access the DB.
 let contactPoints = ['127.0.0.1']; 
-const cassandraClient = getCassandraClient({contactPoints, loadBalancing});
+let loadBalancing = new YCQL.cassandraDriver.policies.loadBalancing.RoundRobinPolicy();
+const ycqlClient = YCQL.getCassandraClient({contactPoints, policies:{loadBalancing}});
+
+let data = {id:1, name: 'Johnny', age: 35, location: {city: 'Tijuana', country: 'Mexiko'}};
+const example = async (client, data) => {
+    try {
+        await async.series([
+            (next) => client.connect(next),
+            (next) => client.execute('CREATE KEYSPACE IF NOT EXISTS ycql_example;', next),
+            (next) => {const createTable = `CREATE TABLE IF NOT EXISTS ycql_example.t1 (id int PRIMARY KEY, name varchar, age int, location jsonb);`;
+                client.execute(createTable, next);
+            },
+            (next) => {
+                const insert = `INSERT INTO ycql_example.t1 (id, name, age, location) VALUES (${data.id}, '${data.name}', ${data.age}, '${JSON.stringify(data.location)}');`;
+                client.execute(insert, next);
+            },
+            (next) => {
+                const select = `SELECT name, age, location FROM ycql_example.t1 WHERE id = ${data.id};`;
+                client.execute(select, function (err, result) {
+                    if (err) {
+                        return next(err);
+                    }
+                    assert(result.rows.length == 1);
+                    let row = result.rows[0];
+                    assert(row.name == data.name);
+                    assert(row.age == data.age);
+                    assert(row.location.city == data.location.city);
+                    assert(row.location.country == data.location.country);
+                    console.log('Data asserted.');
+                    next();
+                });
+            }
+        ]);
+    } finally {
+        await async.series([
+            (next) => {
+                const cleanUp = `DROP TABLE ycql_example.t1;`;
+                client.execute(cleanUp, next);
+            },
+            (next) => client.shutdown(next)
+        ]);
+    }    
+};
+
+example(ycqlClient, data);
--- examples/ysql.js
+++ examples/ysql.js
@@ -4,22 +4,22 @@ const assert = require('assert');
 
 // Make sure the connectionString and credentials allow you to reach your DB.
 let connectionString = "postgres://postgres@localhost:5433/postgres";
-const client = YSQL.getPostgresClient( {connectionString} );
+const ysqlClient = YSQL.getPostgresClient({connectionString});
 
 let data = {id:1, name: 'Johnny', age: 35, city: 'New York'};
-const example = async (data) => {
+const example = async (client, data) => {
     try {
         await async.series([
             (next) => client.connect(next),
-            (next) => {const create_table = `CREATE TABLE person (id int PRIMARY KEY, name varchar, age int, city varchar);`;
-                client.query(create_table, next);
+            (next) => {const createTable = `CREATE TABLE IF NOT EXISTS ysql_example (id int PRIMARY KEY, name varchar, age int, city varchar);`;
+                client.query(createTable, next);
             },
             (next) => {
-                const insert = `INSERT INTO person (id, name, age, city) VALUES (${data.id}, '${data.name}', ${data.age}, '${data.city}');`;
+                const insert = `INSERT INTO ysql_example (id, name, age, city) VALUES (${data.id}, '${data.name}', ${data.age}, '${data.city}');`;
                 client.query(insert, next);
             },
             (next) => {
-                const select = `SELECT name, age, city FROM person WHERE id = ${data.id};`;
+                const select = `SELECT name, age, city FROM ysql_example WHERE id = ${data.id};`;
                 client.query(select, function (err, result) {
                     if (err) {
                         return next(err);
@@ -37,12 +37,12 @@ const example = async (data) => {
     } finally {
         await async.series([
             (next) => {
-                const deletion = `DROP TABLE person;`;
-                client.query(deletion, next);
+                const cleanUp = `DROP TABLE ysql_example;`;
+                client.query(cleanUp, next);
             },
             (next) => client.end(next)
         ]);
     }    
 };
 
-example(data);
+example(ysqlClient, data);
